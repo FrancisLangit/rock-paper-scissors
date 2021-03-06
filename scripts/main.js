a0ca986d71df0bdbcf4cb9baf637a36eb7044edd
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-06 10:00:31 +0800

    Have console print choices made in round

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,11 @@
-function playGame() {
-    /** Runs playRound() five consecutive times. */
-    for (i = 0; i < 5; i++) {
-        console.log(playRound(playerPlay(), computerPlay()));
-    }
-    return "Game over.";
-}
-
-function playRound(playerChoice, computerChoice) {
-    /**Runs a round of the game.*/
-    if (((playerChoice + 1) % 3) == computerChoice) {
-        return "Player 2 won.";
-    } else if (playerChoice == computerChoice) {
-        return "Draw.";
-    } else {
-        return "Player 1 won.";
-    }
-}
+let choices = {
+    "rock": 0,
+    "paper": 1,
+    "scissors": 2,
+};
 
 function playerPlay() {
     /**Prompts choice from user.*/
-    let choices = {
-        "rock": 0,
-        "paper": 1,
-        "scissors": 2,
-    };
     let playerChoice = prompt("Rock, paper, or scissors?");    
     return choices[playerChoice.toLowerCase()];
 }
@@ -31,6 +13,31 @@ function playerPlay() {
 function computerPlay() {
     /**Returns random integer from [0, 1, 2].*/
     return Math.floor(Math.random() * Math.floor(3));
+}
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function playRound(playerChoice, computerChoice) {
+    /**Runs a round of the game.*/
+    console.log(`Player - ${getKeyByValue(choices, playerChoice)}.`);
+    console.log(`Computer - ${getKeyByValue(choices, computerChoice)}.`);
+    if (((playerChoice + 1) % 3) == computerChoice) {
+        return "Computer won.";
+    } else if (playerChoice == computerChoice) {
+        return "Draw.";
+    } else {
+        return "Player won.";
+    }
+}
+
+function playGame() {
+    /** Runs playRound() five consecutive times. */
+    for (i = 0; i < 5; i++) {
+        console.log(playRound(playerPlay(), computerPlay()));
+    }
+    return "Game over.";
 }
 
 console.log(playGame());
