f5c168f1a70a6df5212568f7c702c3108d4d8b7e
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-08 15:43:32 +0800

    Refactor main.js
    Body of code placed into RockPaperScissors class.

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,87 @@
-let choices = {
-    "rock": 0,
-    "paper": 1,
-    "scissors": 2,
-};
-
-function playerPlay() {
-    /**Prompts choice from user.*/
-    let playerChoice = prompt("Rock, paper, or scissors?");    
-    return choices[playerChoice.toLowerCase()];
-}
-
-function computerPlay() {
-    /**Returns random integer from [0, 1, 2].*/
-    return Math.floor(Math.random() * Math.floor(3));
-}
-
-function getKeyByValue(object, value) {
-    /**Gets a key of a dictionary by its value.*/
-    return Object.keys(object).find(key => object[key] === value);
-}
-
-function getRoundWinner(playerChoice, computerChoice) { 
-    /**Returns string of winner of the round.*/
-    if (((playerChoice + 1) % 3) == computerChoice) {
-        return "computer";
-    } else if (playerChoice == computerChoice) {
-        return "draw";
-    } else {
-        return "player";
+class RockPaperScissors {
+    constructor() {
+        this.moves = {
+            "rock": 0,
+            "paper": 1,
+            "scissors": 2,
+        };
+        this.playerScore = 0;
+        this.computerScore = 0;
     }
-}
 
-function playRound(playerChoice, computerChoice) {
-    /**Runs a round of the game.*/
-    return {
-        "playerChoice": getKeyByValue(choices, playerChoice),
-        "computerChoice": getKeyByValue(choices, computerChoice),
-        "roundWinner": getRoundWinner(playerChoice, computerChoice),
-    };
-}
-
-function printRoundResults(round_num, round, playerScore, computerScore) {
-    /**Prints results of round to console.*/
-    console.log(`----- ROUND ${round_num + 1} -----`)
-    console.log(`Player: ${round.playerChoice}`);
-    console.log(`Computer: ${round.computerChoice}`);
-    console.log(`Winner: ${round.roundWinner}`);
-    console.log(`${playerScore} - ${computerScore}`);
-}
-
-function getGameWinner(playerScore, computerScore) {
-    /**Returns winner of the five rounds based on scores.*/
-    if (playerScore > computerScore) {
-        return "Player wins!";
-    } else if (playerScore < computerScore) {
-        return "Computer wins!";
-    } else {
-        return "It's a draw!";
+    getKeyByValue(object, value) {
+        /**Gets dictionary key by its value.*/
+        return Object.keys(object).find(key => object[key] === value);
     }
-}
 
-function playGame() {
-    /**Runs playRound() five consecutive times.*/
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i = 0; i < 5; i++) {
-        round = playRound(playerPlay(), computerPlay());
-        if (round.roundWinner === "player") {
-            playerScore++;
-        } else if (round.roundWinner === "computer") {
-            computerScore++;
+    getPlayerMove() {
+        /**Prompts user's move for round.*/
+        let playerMove = prompt("Rock, paper, or scissors?");    
+        return this.moves[playerMove.toLowerCase()];
+    }
+
+    getComputerMove() {
+        /**Returns random integer from [0, 1, 2] representing computer's move 
+         * for round.*/
+        return Math.floor(Math.random() * Math.floor(3));
+    }
+    
+    getRoundWinner(playerMove, computerMove) { 
+        /**Returns winner of the round.*/
+        if (((playerMove + 1) % 3) == computerMove) {
+            return "computer";
+        } else if (playerMove == computerMove) {
+            return "draw";
+        } else {
+            return "player";
         }
-        printRoundResults(i, round, playerScore, computerScore);
     }
-    return (`-- GAME OVER - ${getGameWinner(playerScore, computerScore)} --`);
+
+    playRound(playerMove, computerMove) {
+        /**Simulates one round of rock-paper-scissors. Returns dictionary
+         * containing of results of round.*/
+        return {
+            "playerMove": this.getKeyByValue(this.moves, playerMove),
+            "computerMove": this.getKeyByValue(this.moves, computerMove),
+            "winner": this.getRoundWinner(playerMove, computerMove),
+        };
+    }
+
+    getResult() {
+        /**Returns string declaring result of game after five rounds.*/
+        if (this.playerScore > this.computerScore) {
+            return "Player wins!";
+        } else if (this.playerScore < this.computerScore) {
+            return "Computer wins!";
+        } else {
+            return "It's a draw!";
+        }
+    }
+    
+    play() {
+        //**Runs playRound five times.*/
+        for (let i = 0; i < 5; i++) {
+            let playerMove = this.getPlayerMove();
+            let computerMove = this.getComputerMove(); 
+            let round = this.playRound(playerMove, computerMove);
+
+            if (round.winner === 'player') {
+                this.playerScore++;
+            } else if (round.winner === 'computer') {
+                this.computerScore++;
+            }
+
+            console.log(`----- ROUND ${i + 1} -----`)
+            console.log(`Player: ${round.playerMove}`);
+            console.log(`Computer: ${round.computerMove}`);
+            console.log(`Winner: ${round.winner}`);
+            console.log(`${this.playerScore} - ${this.computerScore}`);
+        }
+
+        let result = this.getResult(this.playerScore, this.computerScore)
+        return `-- GAME OVER - ${result} --`;
+    }
 }
 
-console.log(playGame());
+let game = new RockPaperScissors(); 
+console.log(game.play());
